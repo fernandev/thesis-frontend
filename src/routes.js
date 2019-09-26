5f49b87a3db9a9d8efcf2bd93908afe401492b55
fernandev <marcos.fernandes@efabrika.com.br>
2019-09-25 23:55:21 -0300

    Add routes.

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+  res.render('home');
+});
+
+router.get('/list', (req, res) => {
+  res.render('list');
+});
+
+router.get('/schedule', (req, res) => {
+  res.render('schedule');
+});
+
 module.exports = router;
