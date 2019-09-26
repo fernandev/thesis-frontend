e7078973a52e52d4b8e814867e221a8fe1982d3d
fernandev <marcos.fernandes@efabrika.com.br>
2019-09-26 02:01:05 -0300

    Fix handlebars table fields. Use database data to fulfill handlebars.

--- src/controllers/list.js
+++ src/controllers/list.js
@@ -1,20 +1,23 @@
-const renderAppointments = (req, res) => {
-  res.render('list', {
-    appointments: [{
-      doctor: "Thalia Cobb",
-      specialty: "Gastroentereologia",
-      patient: "Marcos Fernandes",
-      time: "25/08/2018 20:00",
-      insurance: "Amil"
-    }, {
-      doctor: "Jose Camargo Lacerda",
-      specialty: "Acupuntura",
-      patient: "Marcos",
-      time: "25/08/2019 10:30",
-      insurance: "Amil"
-    }]
-  });
-};
+const { connection } = require('./database');
+
+const renderAppointments = async (req, res) => (
+  fetchAppointments().then(agendamentos => (
+    res.render('list', {
+      agendamentos
+    })
+  )).catch(() => (
+    res.render('list')
+  ))
+);
+
+const fetchAppointments = async () => (
+  connection.query(`CALL list_consultas`)
+    .then(([rows]) => (
+      rows.map(({ medico, especialidade, paciente, data, convenio }) => ({
+        medico, especialidade, paciente, data, convenio
+      }))
+    ))
+);
 
 module.exports = {
   renderAppointments
